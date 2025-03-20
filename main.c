8385888feea0ad1b0b027983d66c67ab64defc26
Serhii <serhi.fedorenko@gmail.com>
2025-03-20 12:43:36 +0000

    Git created

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+const char* enemy1[] = {
+    "  ███  ",
+    " █████ ",
+    "█ ███ █",
+    "███████",
+    " ▀ ▀ ▀ ",
+    NULL
+};
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    return 0;
+}
